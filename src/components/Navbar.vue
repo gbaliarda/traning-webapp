96b7521fefb2026a7e11f7a4571ee9b0f5b2bcc5
gbaliarda <gbaliarda@itba.edu.ar>
2021-09-24 16:48:50 -0300

    First steps

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    
+  </nav>
+  
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {},
+};
+</script>
+
+<style scoped lang="scss">
+  a, .btn {
+    margin-right: 1em;
+  }
+</style>

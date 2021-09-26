09941ddaa589e8bd65d0a98e653b1c5eeb7df2f9
Ezequiel <eperez@itba.edu.ar>
2021-09-26 02:10:27 -0300

    Routine, exercises, create routine pages

---
+++ src/components/CreateCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="ejercicio">
+    <p class="card-title">{{ titulo }}</p>
+    <div class="descripcion">
+      <p>Repeticiones: {{ repeticiones }}</p>
+      <p>Series: {{ series }}</p>
+    </div>
+    <button>Detalles</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateCard",
+  props: {
+    titulo: String,
+  },
+  data() {
+    return {
+      repeticiones: 15,
+      series: 3,
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .ejercicio {
+    padding: 2em 1em;
+    width: 250px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    margin-right: 3em;
+    margin-bottom: 2em;
+
+    .card-title {
+      font-size: 1.4em;
+      text-align: center;
+    }
+
+    .descripcion {
+      p {
+        padding: 5px;
+      }
+    }
+
+    button {
+      color: #DA611B;
+      border: 1px solid #DA611B;
+      margin: 0 auto;
+      width: 100px;
+      height: 40px;
+      border-radius: 10px;
+    }
+  }
+</style>
---
+++ src/components/RoutineCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="rutina">
+    <p class="card-title">{{ titulo }}</p>
+    <div class="descripcion">
+      <p>Duracion: {{ duracion }}</p>
+      <p>Dificultad: {{ dificultad }}</p>
+    </div>
+    <div class="buttons">
+      <button>Detalles</button>
+      <v-icon class="icon" size="34">mdi-share-variant</v-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoutineCard",
+  props: {
+    titulo: String,
+    duracion: String,
+    dificultad: String,
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .rutina {
+    padding: 2em 1em;
+    width: 250px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    margin-right: 3em;
+    margin-bottom: 2em;
+
+    .card-title {
+      font-size: 1.4em;
+      text-align: center;
+    }
+
+    .descripcion {
+      p {
+        padding: 5px;
+      }
+    }
+
+    .buttons {
+      display: flex;
+      justify-content: space-between;
+      width: 95%;
+      margin: 0 auto;
+
+      button {
+        color: #DA611B;
+        border: 1px solid #DA611B;
+        width: 100px;
+        height: 40px;
+        border-radius: 10px;
+      }
+
+      .icon {
+        cursor: pointer;
+        color: #DA611B;
+      }
+    }
+  }
+</style>

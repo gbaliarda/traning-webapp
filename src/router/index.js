2cfb2b3ad54df04e75dedee532865e8074e32c2e
Agustín Morantes <agustinmorantes@gmail.com>
2021-09-26 01:43:22 -0300

    Profile page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Profile from "../views/Profile.vue";
 
 Vue.use(VueRouter);
 
@@ -19,6 +20,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
+  }
 ];
 
 const router = new VueRouter({
